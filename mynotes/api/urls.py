3b72ca379bcfb9a52257e1ea1f58876ed26e3968
zalduss <seanphillips44@gmail.com>
2024-06-06 00:56:10 -0700

    initial commit

---
+++ mynotes/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
+
+urlpatterns = [
+    path('user/register/', CreateUserView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('notes/', views.NoteListCreate.as_view(), name='note-list'),
+    path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name='delete-note'),
+
+]
